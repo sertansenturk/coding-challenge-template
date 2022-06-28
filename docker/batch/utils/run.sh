76ff9d4865fbc3c7aefe52ed7881a82efb14b191
Sertan Şentürk <sertansenturk@gmail.com>
2022-06-28 23:00:20 +0000

    Rename app to batch for clarity

--- docker/batch/utils/run.sh
+++ docker/batch/utils/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e # Any subsequent commands which fail will cause the shell script to exit immediately
 
-ct --config ./docker/app/config.yaml --key1 value1 run_all
+ct --config ./docker/batch/config.yaml --key1 value1 run_all
